bd88ad57b65cf075da3e4a6e9c17e64265cbb353
Sam Shearer <sshearer101@gmail.com>
2021-07-27 14:58:14 -0600

    commit tree 53f069135619da3c113261745746df9b5227df7a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+
+const imgDiv = document.getElementById('dog-image-container');
+const breedUl = document.getElementById('dog-breeds')
+
+
+fetch(imgUrl)
+    .then((res) => res.json())
+    .then((data) => addImageElements(data.message))
+
+
+fetch(breedUrl)
+    .then((res) => res.json())
+    .then((data) => addBreedListItems(data.message));
+
+
+function addImageElements(imgArr) {
+    imgArr.forEach(addImage);
+}
+
+function addImage(imgUrl){
+    const dogImg = document.createElement('img');
+    dogImg.src = imgUrl;
+    dogImg.classList.add('dog-image');
+    imgDiv.append(dogImg);
+}
+
+function addBreedListItems(itemObject) {
+    const itemKeys = Object.keys(itemObject);
+    itemKeys.forEach((item) => addBreedListItems(item, itemObject[item]));
+}
+
+function addBreedItem(breed, subBreeds) {
+    addBreedListItems(breed);
+    subBreeds.forEach(sb => addBreed(`${sb} ${breed}`));
+}
